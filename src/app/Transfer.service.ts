50600042f6907656fd38e9aff5dc617dac9f2c66
mschneiderbauer <33754127+mschneiderbauer@users.noreply.github.com>
2017-12-19 10:20:54 +0100

    updated table

--- src/app/Transfer.service.ts
+++ src/app/Transfer.service.ts
@@ -3,6 +3,6 @@ import { Transfer } from './Transfer';
 export class GetTransfers{
     getTransfers():Transfer[]{
         
-        return [new Transfer(1,'t1',11), new Transfer(2,'t2',22),new Transfer(2,'t2',22)];
+        return [new Transfer('20.12.2017','t1',11), new Transfer('21.12.2017','t2',22),new Transfer('22.12.2017','t2',22)];
     }
 }
