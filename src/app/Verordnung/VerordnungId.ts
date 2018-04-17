751ba696b621b5cbb2428c3cbc8a341e9bb8529c
jubu1812 <33754507+jubu1812@users.noreply.github.com>
2018-04-17 08:40:38 +0200

    Umbennung Date, VerordnungsId Klasse

---
+++ src/app/Verordnung/VerordnungId.ts
@@ -0,0 +1,9 @@
+export class VerordnungsId{      
+
+    constructor(
+        public kundennummer:number,
+        public vo_id: number
+    )
+    {}
+    
+}
--- src/app/Verordnung/verordnung.ts
+++ src/app/Verordnung/verordnung.ts
@@ -3,13 +3,13 @@ import { Leistung } from "app/Verordnung/Leistung";
 import { Bewilligung } from "app/Verordnung/Bewilligung";
 import { Leistungserbringer } from "app/Verordnung/Leistungserbringer";
 import { Leistungserbringung } from "app/Verordnung/Leistungserbringung";
+import { VerordnungsId } from "./VerordnungId";
 
 export class Verordnung {
-    public vo_id: number;
+    public id: VerordnungsId;
     public sendungs_id:number;
 
-    constructor(
-        public kundennummer: number,       
+    constructor(       
         public kostentraeger_id: string,
         public vpnrv: string,
         public zunav: string,
